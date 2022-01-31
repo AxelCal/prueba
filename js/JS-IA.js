0094f2e9084e6a6a735ef299b0c31ac04dd63749
AxelCal <axel94778@gmail.com>
2022-01-31 10:45:55 -0600

    mi primer commit

---
+++ js/JS-IA.js
@@ -0,0 +1,11 @@
+function iniciarMap(){
+  var coord = {lat:19.642780 ,lng: -99.125975};
+  var map = new google.maps.Map(document.getElementById('map'),{
+    zoom: 15,
+    center: coord
+  });
+  var marker = new google.maps.Marker({
+    position: coord,
+    map: map
+  });
+}
